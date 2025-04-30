5482edf992ce9440fe757a5a19cb1a3d55b471a2
Zaim Irfan <zaim8797@gmail.com>
2025-04-30 11:52:58 +0800

    NuxtUI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,11 @@ export default defineNuxtConfig({
 
   future: {
     compatibilityVersion: 4,
-  },
-
-  compatibilityDate: '2024-11-01',
+  }, 
   
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: ['@nuxt/ui'],
+
+  css: ['~/assets/css/main.css'],
 })
